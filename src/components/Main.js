0cb42837608043064bd6cad38587009e8e1166be
Andre <andreivanp@gmail.com>
2022-09-14 21:37:30 +0700

    add feature

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <div class="mt-0 p-5 text-white rounded">
+      <h1 className="text-center">Nonton gratis,</h1>
+      <h1 className="text-center">Gak pake ribet</h1>
+    </div>
+  );
+}
+
+export default Main;
