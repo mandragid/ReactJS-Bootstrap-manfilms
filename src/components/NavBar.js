09b92d0209f10a8916a479d087429bbe37bad728
Andre <andreivanp@gmail.com>
2022-11-04 09:50:47 +0700

    bug fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,21 @@
 import Navbar from "react-bootstrap/Navbar";
-import Logo from "/Users/user/Desktop/mandragidmovies/src/img/icon/logo.png";
+import Logo from "../img/icon/logo.png";
 import Button from "react-bootstrap/Button";
 function NavigationBar() {
-  return (
-    <div>
-      <Navbar expand="lg d-flex">
-        <Navbar.Brand href="#home" className="mt-1 ms-5">
-          <img
-            alt="Mandragid"
-            src={Logo}
-            width="auto"
-            height="auto"
-            className="d-inline-block align-top"
-          />{" "}
-        </Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          <Button variant="danger" className="float-end">
-            Sign In
-          </Button>{" "}
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar expand="lg d-flex">
+				<Navbar.Brand href="#home" className="mt-1 ms-5">
+					<img alt="Mandragid" src={Logo} width="auto" height="auto" className="d-inline-block align-top" />{" "}
+				</Navbar.Brand>
+				<Navbar.Collapse className="justify-content-end">
+					<Button variant="danger" className="float-end">
+						Sign In
+					</Button>{" "}
+				</Navbar.Collapse>
+			</Navbar>
+		</div>
+	);
 }
 
 export default NavigationBar;
