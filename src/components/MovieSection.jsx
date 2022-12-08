a70ace2e3c21a321bef1755b5d2cb1b0e167557a
Andre <andreivanp@gmail.com>
2022-12-08 15:55:51 +0700

    switch API

--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -7,10 +7,11 @@ import axios from "axios";
 
 const MovieSection = () => {
 	const [movieData, setMoviedata] = useState({});
+	const entries = Object.entries(movieData);
 
 	useEffect(() => {
 		axios
-			.get("https://api.themoviedb.org/3/movie/2?api_key=23942a5fde32780928ffa5dfd8a92f2d")
+			.get("https://api.themoviedb.org/3/movie/popular?api_key=23942a5fde32780928ffa5dfd8a92f2d")
 			.then((res) => {
 				setMoviedata(res.data);
 				console.log(res);
@@ -21,14 +22,16 @@ const MovieSection = () => {
 	return (
 		<div className="container-fluid">
 			<div className="container">
-				<Card style={{ width: "18rem" }}>
-					<Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} />
-					<Card.Body>
-						<Card.Title>{movieData.original_title}</Card.Title>
-						<Card.Text>1</Card.Text>
-						<Button variant="primary">Go somewhere</Button>
-					</Card.Body>
-				</Card>
+				{Object.entries(movieData).length ? (
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${movieData.results[1].poster_path}`} />
+						<Card.Body>
+							<Card.Title>{movieData.original_title}</Card.Title>
+							<Card.Text>1</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				) : null}
 			</div>
 		</div>
 	);
