b49785a575aed2c7a7ea5d613f40a0e720e9c18f
Mandragid <mandragid@MacBook-Pro-Macbook.local>
2022-12-09 00:03:19 +0700

    update api

--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -6,35 +6,47 @@ import { useEffect } from "react";
 import axios from "axios";
 
 const MovieSection = () => {
-	const [movieData, setMoviedata] = useState({});
-	const entries = Object.entries(movieData);
+  const [movieData, setMoviedata] = useState([]);
+  const entries = Object.entries(movieData);
 
-	useEffect(() => {
-		axios
-			.get("https://api.themoviedb.org/3/movie/popular?api_key=23942a5fde32780928ffa5dfd8a92f2d")
-			.then((res) => {
-				setMoviedata(res.data);
-				console.log(res);
-			})
-			.catch((err) => console.log(err.message));
-	}, []);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=23942a5fde32780928ffa5dfd8a92f2d"
+      )
+      .then((res) => {
+        setMoviedata(res.data.results);
+        console.log(res);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
 
-	return (
-		<div className="container-fluid">
-			<div className="container">
-				{Object.entries(movieData).length ? (
-					<Card style={{ width: "18rem" }}>
-						<Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${movieData.results[1].poster_path}`} />
-						<Card.Body>
-							<Card.Title>{movieData.original_title}</Card.Title>
-							<Card.Text>1</Card.Text>
-							<Button variant="primary">Go somewhere</Button>
-						</Card.Body>
-					</Card>
-				) : null}
-			</div>
-		</div>
-	);
+  return (
+    <div className="container-fluid d-flex">
+      <div className="container">
+        {Object.entries(movieData).length
+          ? movieData.map((item) => (
+              <div className="row">
+                <div className="col-6">
+                  {" "}
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img
+                      variant="top"
+                      src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                    />
+                    <Card.Body>
+                      <Card.Title>{item.original_title}</Card.Title>
+                      <Card.Text>{item.overview}</Card.Text>
+                      <Button variant="primary">Detail</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
+  );
 };
 
 export default MovieSection;
